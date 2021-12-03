92e4d84613d1cba6869ecb76abf2c750bfc6089e
seham-aweda <awedaseham@gmail.com>
2021-12-03 11:01:03 +0200

    env

--- Models/user.model.js
+++ Models/user.model.js
@@ -90,7 +90,7 @@ userSchema.methods.toJSON = function () {
 
 userSchema.methods.generateAuthToken=async function (){
     const user=this
-    const token=jwt.sign({_id:user._id.toString()}, `${process.env.JWT_SECRET_KEY}`)
+    const token=jwt.sign({_id:user._id.toString()}, `${process.env.JWT_SECRET_KEY}`,{expiresIn: '8h'})
 
     user.tokens=user.tokens.concat({token})
     await user.save()
